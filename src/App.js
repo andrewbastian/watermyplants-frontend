774fd9b376104aec84eb490e3f385dcd44a0f3a2
andrewbastian <andrew.bastian@gmail.com>
2021-05-22 16:44:25 -0700

    Cleaned up unused components in App.js

--- src/App.js
+++ src/App.js
@@ -10,15 +10,8 @@ import Register from './components/Register';
 import Profile from './components/Profile';
 
 import Dashboard from './components/Dashboard';
-import PlantCard from './components/PlantCard';
 import AddPlant from './components/AddPlant';
 
-import AddPlantWater from './components/AddPlantWater';
-
-import EditPlant from './components/EditPlant';
-import EditWater from './components/EditWater';
-
-import SuccessSnackbar from './components/Notifications';
 
 const App = ({ history  }) => (
       <ConnectedRouter history={history}>
@@ -28,16 +21,8 @@ const App = ({ history  }) => (
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <PrivateRoute path="/account" component={Profile} />
-            <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <PrivateRoute path="/plant/add" component={AddPlant} />
-            <PrivateRoute exact path="/plant/:id/water" component={AddPlantWater} />
-            <PrivateRoute exact path="/plant/:id" component={PlantCard} />
-            <PrivateRoute
-              path="/plant/notifications"
-              component={<SuccessSnackbar />}
-            />
-            <PrivateRoute path="/plant/:id/edit" component={EditPlant} />
-            <PrivateRoute path="/plant/:id/water/edit" component={EditWater} />
+            <PrivateRoute path="/dashboard" component={Dashboard} />
           </Switch>
         </div>
       </ConnectedRouter>
