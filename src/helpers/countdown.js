69e516aa6fa6762bad57c8cfe54c06ea1be324c6
andrewbastian <andrew.bastian@gmail.com>
2021-05-23 12:46:40 -0700

    Moved App into firebase functions folder

--- src/helpers/countdown.js
+++
@@ -1,6 +0,0 @@
-export function countdown(nextWatering) {
-  const  oneDay = 24*60*60*1000 
-    const today = new Date()
-    const subtract = new Date(nextWatering)
-return Math.ceil((subtract.getTime() - today.getTime()) / oneDay - 2)
-}
--- src/utils/token.js
+++
@@ -1,1 +0,0 @@
-export const hasToken = localStorage.hasOwnProperty("token");
